d845eb85d814243808cdf1f82ca3033fabc8011f
maxxthur <46267997+maxxthur@users.noreply.github.com>
2022-09-19 09:57:43 +0200

    na_detect, na_summary
    Added functions na_detect, na_summary:

---
+++ mini.helpers/.Rproj.user/58F33D1C/sources/session-020F17BA/1EE12E55-contents
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+na_detect <- function(x) {
+  x %>%
+    filter_all(any_vars(is.na(.)))
+}
---
+++ mini.helpers/R/na_summary.R
@@ -0,0 +1,59 @@
+#' Title
+#'
+#' @param x
+#' @param group_by
+#'
+#' @return
+#' @export
+#'
+#' @examples
+na_summary <- function(x, group_by = NULL) {
+
+  # counting
+  if(is.null(group_by)) {
+    nas <- x %>%
+      mutate(across(.fns = is.na)) %>%
+      summarise(across(.fns = sum))
+  } else {
+    nas <- x %>%
+      group_by(across(all_of(group_by))) %>%
+      mutate(across(.fns = is.na)) %>%
+      summarise(across(.fns = sum))
+  }
+
+  # plot
+
+  if(is.null(group_by)) {
+    plot <- nas %>%
+      pivot_longer(cols = everything(),
+                   names_to = "var",
+                   values_to = "value") %>%
+      ggplot(aes(x = var, y = value)) +
+      geom_col(fill = "steelblue") +
+      labs(y = "number of NA") +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, face = "bold"),
+            strip.text = element_text(face = "bold"))
+  } else {
+    rhs <- paste(group_by, collapse = "+")
+
+    form <- paste0(". ~", rhs)
+
+    plot <- nas %>%
+      pivot_longer(cols = -all_of(group_by),
+                   names_to = "var",
+                   values_to = "value") %>%
+      ggplot(aes(x = var, y = value)) +
+      geom_col(fill = "steelblue") +
+      labs(y = "number of NA") +
+      facet_wrap(form) +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, face = "bold"),
+            strip.text = element_text(face = "bold"))
+
+  }
+
+  print(plot)
+  return(nas)
+
+}
