2f4ba98ff2c5b67f6f377411f66ba3e4e1ed5146
maxxthur <46267997+maxxthur@users.noreply.github.com>
2022-09-21 11:47:23 +0200

    added pkg_excel and pkg_dw
    load packages for data wrangling and interfaces to MS Excel

---
+++ mini.helpers/.Rproj.user/58F33D1C/sources/session-6DC70A6F/E1BA01A0-contents
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+pkg_excel <- function() {
+  pacman::p_load("readxl",
+                 "writexl",
+                 "openxlsx",
+                 "tidyxl")
+}
---
+++ mini.helpers/R/packages_data_wrangling.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+pkg_dw <- function() {
+  pacman::p_load("tidyverse",
+                 "data.table",
+                 "lubridate",
+                 "janitor")
+}
